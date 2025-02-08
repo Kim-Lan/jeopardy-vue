9f420bf4263138819f2809797fc2c2743210eccc
Kim-Lan <me@kim-lan.com>
2025-02-08 00:44:28 -0800

    Add styling

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -32,17 +32,45 @@ function toggleAnswer() {
 </script>
 
 <template>
-  <main>
-    <Button @click="$router.back()">Back</Button>
-    <h2>{{ category.name }}, {{ questionId }} Points</h2>
-    <h3>{{ question.question }}</h3>
-    <img v-if="question.image" :src="question.image.src" :alt="question.image.alt" />
-    <audio v-if="question.audio" :src="question.audio" controls></audio>
-    <Button @click="toggleAnswer">
+  <main class="w-full md:w-3/4 lg:w-1/2 mx-auto px-4 flex flex-col justify-center text-center gap-8">
+    <Button
+      @click="$router.back()"
+      class="self-start"
+    >
+      Back
+    </Button>
+
+    <h2 class="text-xl">
+      {{ category.name }}, {{ questionId }} Points
+    </h2>
+
+    <h3 class="text-2xl font-bold">
+      {{ question.question }}
+    </h3>
+
+    <img
+      v-if="question.image"
+      :src="question.image.src"
+      :alt="question.image.alt"
+      class="mx-auto"
+    />
+
+    <audio
+      v-if="question.audio"
+      :src="question.audio"
+      controls
+      class="mx-auto"
+    ></audio>
+
+    <Button
+      @click="toggleAnswer"
+      class="mx-auto"
+    >
       <span v-if="!isAnswerVisible">Show</span>
       <span v-else>Hide</span>
        Answer
     </Button>
+
     <div v-if="isAnswerVisible">
       {{ question.answer }}
     </div>
